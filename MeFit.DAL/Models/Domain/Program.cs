fab47f7b2b662396addb1595c778505d11ec73ec
Vilyana Angelova <angelova0424@gmail.com>
2022-03-08 13:42:12 +0100

    FIX: fixed all models to show correct entities and relationships

--- MeFit.DAL/Models/Domain/Program.cs
+++ MeFit.DAL/Models/Domain/Program.cs
@@ -1,9 +1,16 @@
-﻿namespace MeFitAPI.DAL.Models.Domain
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace MeFitAPI.DAL.Models.Domain
 {
     public class Program
     {
         public int Id { get; set; }
+        [Required, MaxLength(70)]
         public string Name { get; set; }
+        [Required, MaxLength(70)]
         public string Category { get; set; }
+        //navigation property
+        public ICollection<Workout> Workouts { get; set; } = new HashSet<Workout>();
     }
 }
--- MeFit.DAL/Models/Domain/Workout.cs
+++ MeFit.DAL/Models/Domain/Workout.cs
@@ -1,11 +1,18 @@
-﻿namespace MeFitAPI.DAL.Models.Domain
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace MeFitAPI.DAL.Models.Domain
 {
     public class Workout
     {
         public int Id { get; set; }
+        [Required, MaxLength(70)]
         public string Name { get; set; }
         public string Type { get; set; }
-        public int Complete { get; set; }
-
+        public bool Complete { get; set; }
+        //navigation properties
+        public ICollection<Set> Sets { get; set; } = new HashSet<Set>();
+        public ICollection<Program> Programs { get; set; } = new HashSet<Program>();
+        public ICollection<GoalWorkout> Goals { get; set; } = new HashSet<GoalWorkout>();
     }
 }
