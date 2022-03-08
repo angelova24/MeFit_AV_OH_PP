6ae43f7de1c53dd0827110af9145db6eab775bdf
Petar Petrov <petar.dragov.petrov@gmail.com>
2022-03-08 16:43:48 +0100

    FEAT: Created User DTO's WIP implementation

---
+++ MeFit.DAL/Models/DTOs/UserDTOs/UserCreateDTO.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MeFit.DAL.Models.DTOs
+{
+    public class UserCreateDTO
+    {
+       
+        [Required, MaxLength(50)]
+        public string Username { get; set; }
+        [Required, MaxLength(30)]
+        public string Password { get; set; }
+        [Required, MaxLength(50)]
+        public string FirstName { get; set; }
+        [Required, MaxLength(50)]
+        public string LastName { get; set; }
+
+        //WIP needs to be commented with team
+
+        //public bool IsContributor { get; set; }
+        //public bool IsAdmin { get; set; }
+    }
+}
---
+++ MeFit.DAL/Models/DTOs/UserDTOs/UserDeleteDTO.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MeFit.DAL.Models.DTOs.UserDTOs
+{
+    public class UserDeleteDTO
+    {
+        public int Id { get; set; }
+        [Required, MaxLength(50)]
+        public string Username { get; set; }
+    }
+}
