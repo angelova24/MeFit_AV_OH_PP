7ab25b97cff25b422693f5df62b8303246c7c371
Vilyana Angelova <angelova0424@gmail.com>
2022-03-09 14:29:34 +0100

    FEAT: added DTOs for exercises

---
+++ MeFit.DAL/Models/DTOs/Exercise/ExerciseCreateDTO.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MeFit.DAL.Models.DTOs.Exercise
+{
+    public class ExerciseCreateDTO
+    {
+        [Required, MaxLength(70)]
+        public string Name { get; set; }
+        [MaxLength(200)]
+        public string Description { get; set; }
+        [Required, MaxLength(70)]
+        public string TargetMuscleGroup { get; set; }
+        [MaxLength(200)]
+        public string ImageURL { get; set; }
+        [MaxLength(200)]
+        public string VideoURL { get; set; }
+    }
+}
