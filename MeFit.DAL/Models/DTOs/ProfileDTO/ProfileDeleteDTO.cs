54020bd6aedbeab8d088bbdd3bb9dcacd6f44206
Petar Petrov <petar.dragov.petrov@gmail.com>
2022-03-10 09:33:14 +0100

    FIX: Removed MaxLenght in models because double does not have a maxlenght

--- MeFit.DAL/Models/DTOs/ProfileDTO/ProfileDeleteDTO.cs
+++ MeFit.DAL/Models/DTOs/ProfileDTO/ProfileDeleteDTO.cs
@@ -5,12 +5,6 @@ namespace MeFit.DAL.Models.DTOs.ProfileDTO
     public class ProfileDeleteDTO
     {
         public int Id { get; set; }
-        public double Weight { get; set; }
-        [MaxLength(10)]
-        public double Height { get; set; }
-        [MaxLength(100)]
-        public string MedicalConditions { get; set; }
-        [MaxLength(100)]
-        public string Disabilities { get; set; }
+        
     }
 }
