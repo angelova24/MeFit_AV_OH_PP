e252192c6f54e90b77797d5399a8508dcc6f4e4b
Tranquillo1811 <oliver.hauck@de.experis.com>
2022-03-10 06:31:38 +0100

    CHORE: removes obsolete folders

--- mefit-vuejs-frintend/src/main.js
+++
@@ -1,9 +0,0 @@
-import { createApp } from 'vue';
-import App from './App.vue';
-import router from "./components/router"
-import store from "./components/store"
-
-createApp(App)
-    .use(router)
-    .use(store)
-    .mount('#app');
