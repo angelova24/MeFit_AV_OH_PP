f4073bb526f44478fa42f5641aee77ca802d08a7
Vilyana Angelova <angelova0424@gmail.com>
2022-03-10 11:18:24 +0100

    CHORE: small fixes on domain models

--- MeFit.DAL/Models/Domain/Exercise.cs
+++ MeFit.DAL/Models/Domain/Exercise.cs
@@ -9,7 +9,7 @@ namespace MeFit.DAL.Models.Domain
         public int Id { get; set; }
         [Required, MaxLength(70)]
         public string Name { get; set; }
-        [MaxLength(200)]
+        [MaxLength(250)]
         public string Description { get; set; }
         [Required, MaxLength(70)]
         public string TargetMuscleGroup { get; set; }
