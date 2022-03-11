2a2593ad2c3ecfdbbe1fdd95da50394c241d922c
Vilyana Angelova <angelova0424@gmail.com>
2022-03-11 14:46:20 +0100

    FEAT: added mapping file for Program

---
+++ MeFit.API/Profiles/ProgramProfile.cs
@@ -0,0 +1,17 @@
+﻿using MeFit.DAL.Models.Domain;
+using MeFit.DAL.Models.DTOs.Program;
+using System.Linq;
+
+namespace MeFit.API.Profiles
+{
+    public class ProgramProfile : AutoMapper.Profile
+    {
+        public ProgramProfile()
+        {
+            CreateMap<Program, ProgramReadDTO>()
+               .ForMember(pdto => pdto.Workouts, opt =>
+             opt.MapFrom(p => p.Workouts.Select(w => w.Id).ToArray()))
+               .ReverseMap();
+        }
+    }
+}
