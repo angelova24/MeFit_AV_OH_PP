d37324cd2f30347f22764d5a0a2c66408bd91c9c
Vilyana Angelova <angelova0424@gmail.com>
2022-03-11 11:05:24 +0100

    FEAT: added DTOs for WorkoutSet relationship

---
+++ MeFit.DAL/Models/DTOs/Set/SetReadDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace MeFit.DAL.Models.DTOs.Set
+{
+    public class SetReadDTO
+    {
+        public int ExerciseId { get; set; }
+        public int ExerciseRepetitions { get; set; }
+    }
+}
