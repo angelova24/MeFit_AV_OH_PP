1fecb518f9a4f75990d1f95a7a84e083262dcd3a
Tranquillo1811 <oliver.hauck@de.experis.com>
2022-03-22 12:16:24 +0100

    FEAT: implements undesigned Dashboard

--- mefit-vuejs-frontend/src/components/Goals/GoalsDetail.vue
+++ mefit-vuejs-frontend/src/components/Goals/GoalsDetail.vue
@@ -1,8 +1,8 @@
 <script setup>
     import { toRefs, computed, ref } from 'vue';
     import { useStore } from 'vuex';
-    import WorkoutList from '../Workouts/WorkoutList.vue';
-import GoalWorkoutList from './GoalWorkoutList.vue';
+    //import WorkoutList from '../Workouts/WorkoutList.vue';
+    import GoalWorkoutList from './GoalWorkoutList.vue';
 
     const props = defineProps({
         goal: {
@@ -15,21 +15,7 @@ import GoalWorkoutList from './GoalWorkoutList.vue';
     const store = useStore();
     const goalStartDateString = ref(goal.value.startDate.toDateString());
     const goalEndDateString = ref(goal.value.endDate.toDateString());
-    const workouts = computed(() => {
-        const tempWorkouts = [];
-        if (goal.value.workouts !== undefined)
-        {
-            console.log("Workouts", goal.value.workouts);
-            for (const workout of goal.value.workouts) {
-                console.log("workoutId:", workout.workoutId);
-                const tempWorkout = store.getters.getWorkoutById(workout.workoutId);
-                console.log("tempWorkout:", tempWorkout);
-                tempWorkouts.push(tempWorkout);
-            }
-        }
-        console.log(tempWorkouts);
-        return tempWorkouts;
-    });
+    
 </script>
 
 <template>
@@ -43,8 +29,7 @@ import GoalWorkoutList from './GoalWorkoutList.vue';
             </section>
             <section title="workouts">
                 This goal comprises the following workouts:
-                <!-- <WorkoutList v-if="workouts[0] !== undefined" v-bind:workouts="workouts"></WorkoutList> -->
-                <GoalWorkoutList v-if="goal.workouts !== undefined" v-bind:goalworkouts="goal.workouts"></GoalWorkoutList>
+                <GoalWorkoutList  v-if="JSON.stringify(goal.workouts) !== '[]'" v-bind:goalworkouts="goal.workouts"></GoalWorkoutList>
             </section>
         </main>
     </div>
